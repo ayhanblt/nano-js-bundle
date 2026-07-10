d7efe1b73d892b6aac3cf0c58a2f140dc5c898b6
ayhanblt <ayhanbulut91@gmail.com>
2026-07-10 10:40:23 +0300

    update: added options for browser access

--- api/chat.ts
+++ api/chat.ts
@@ -1,6 +1,18 @@
 import { GeminiBackendProvider } from './providers/GeminiBackendProvider.js';
 
+function setCorsHeaders(res: any) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+}
+
 export default async function handler(req: any, res: any) {
+  setCorsHeaders(res);
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
   // Only allow POST requests
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
